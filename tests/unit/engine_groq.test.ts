a8c40d77205ece78c9178e56948c72cb5fa62745
Nicolas Bonamy <nicolas@bonamy.fr>
2024-12-06 12:56:48 -0600

    groq llama 3.3

--- tests/unit/engine_groq.test.ts
+++ tests/unit/engine_groq.test.ts
@@ -44,11 +44,11 @@ beforeEach(() => {
 test('Groq Load Models', async () => {
   const models = await loadGroqModels(config)
   expect(models.chat).toStrictEqual([
+    { id: 'llama-3.3-70b-versatile', name: 'Llama 3.3 70B' },
     { id: 'llama-3.2-90b-vision-preview', name: 'Llama 3.2 90B Vision (Preview)' },
     { id: 'llama-3.2-11b-vision-preview', name: 'Llama 3.2 11B Vision (Preview)' },
     { id: 'llama-3.2-3b-preview', name: 'Llama 3.2 3B Text (Preview)' },
     { id: 'llama-3.2-1b-preview', name: 'Llama 3.2 1B Text (Preview)' },
-    { id: 'llama-3.1-70b-versatile', name: 'Llama 3.1 70b' },
     { id: 'llama-3.1-8b-instant', name: 'Llama 3.1 8b' },
     { id: 'llama3-70b-8192', name: 'Llama 3 70b' },
     { id: 'llama3-8b-8192', name: 'Llama 3 8b' },
