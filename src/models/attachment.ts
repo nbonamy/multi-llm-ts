f5e7a898545782ddb992c6bfe738d6eb7415bcd3
Nicolas Bonamy <nicolas@bonamy.fr>
2025-11-29 09:58:37 -0600

    feat: more test formats

--- src/models/attachment.ts
+++ src/models/attachment.ts
@@ -1,8 +1,21 @@
 
 export const textFormats = [
-  'txt', 'csv', 'pdf', 'docx', 'pptx', 'xlsx',
-  'json', 'js', 'html', 'css',
-  'xml', 'yaml', 'yml', 'md'
+  'txt', 'csv', 'pdf', 'docx', 'pptx', 'xlsx', 'md',
+  'html', 'htm', 'css', 'scss', 'sass', 'less',
+  'js', 'jsx', 'ts', 'tsx', 'mjs', 'cjs', 'vue', 'svelte',
+  'py', 'pyw', 'pyx', 'pyi',
+  'java', 'kt', 'kts', 'groovy', 'scala',
+  'c', 'h', 'cpp', 'cc', 'cxx', 'hpp', 'hh', 'hxx', 'cs',
+  'swift', 'm', 'mm',
+  'rs', 'go', 'zig',
+  'rb', 'php', 'pl', 'lua', 'r',
+  'sh', 'bash', 'zsh', 'fish', 'ps1', 'bat', 'cmd',
+  'json', 'xml', 'yaml', 'yml', 'toml', 'ini', 'env',
+  'properties', 'conf', 'config', 'cfg',
+  'gitignore', 'gitattributes', 'editorconfig', 'npmrc', 'nvmrc',
+  'sql', 'psql', 'mysql',
+  'gradle', 'maven', 'cmake', 'make', 'dockerfile',
+  'log', 'diff', 'patch'
 ]
 export const imageFormats = [ 'jpeg', 'jpg', 'png', 'webp' ]
 
